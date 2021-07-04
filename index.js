9dcfa2b680de6fa0020ab5d2a7e945319c708087
Samuel <Samuelpmontague@gmail.com>
2021-07-03 22:06:12 -0500

    completed index.js file

--- index.js
+++ index.js
@@ -1,13 +1,97 @@
-// TODO: Include packages needed for this application
 
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateReadme = require("./utils/generateReadme")
+
+const writeFileAsync = util.promisify(fs.writeFile);
 // TODO: Create an array of questions for user input
-const questions = [];
+function questions() {
+       return inquirer.prompt([
+        {
+        type: "input",
+        message: "what is the title of the project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the project about? Give a detailed description!",
+        name: "Description"
+    },
+    {   
+        type: "input",
+        message: "Table of Contents",
+        name: "Table of Contents"   
+    },
+    {
+        type: "input",
+        message: "How is the app used? Give Instructions!",
+        name: "usage"
+    },
+    {
+        type:"list",
+        message: "What license is being used?",
+        name: "License",
+        choices: [
+            "MIT",
+            "Apache 2.0",
+        ]
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installations",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "Who is contributing to the project?",
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "What commands are needed to test this app?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Contact info for questions.",
+        name: "questions"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name:"email"
+    }
+]);
+}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+
+
+//TODO: Create a function to initialize app
+async function init(){
+    try{
+        const answers = await questions();
+        const generateContent = generateReadme(answers);
+
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log(' successfully wrote to README.md')
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `
+  <h1 align="center" ${data.title}
+  ![GitHub license](https://img.shields.io/badge/license-APACHE 2.0-blue.svg)
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+  
+  ## Description
+  ${data.description}
+    
+    ## Table of Contents 
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## Installation
+    To install necessary dependencies, run the following command:
+    \`\`\`${data.installations}\`\`\`
+    ## Usage
+    ${data.usage}
+    ## License
+    ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+    <br />
+    This application is covered by the ${data.license} license
+    ${data.license}
+    ## Contributing
+    ${data.contribute}
+    ## Tests
+    To run tests, run the following command:
+    \`\`\`${data.tests} \`\`\`
+    ## Questions
+    If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at (https://github.com/${data.username}/).`
 }
 
 module.exports = generateMarkdown;
